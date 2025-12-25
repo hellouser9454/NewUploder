0881143ff342eb2f945464977ee1934ea28dc459
hellouser9454 <github9454161@gmail.com>
2025-12-25 16:55:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", ""))
+OWNER = int(environ.get("OWNER", "8272089956"))
 CREDIT = environ.get("CREDIT", "")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
@@ -20,3 +20,4 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
